190051ba57a5a139f78371c304821a3c41356ba6
Sanhanat Ngamchaiwong <sanhanat.ngam@gmail.com>
2021-01-23 12:45:56 +0700

    add gitignore and set capabilities

--- SampleUniversalLink/ContentView.swift
+++ SampleUniversalLink/ContentView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
+        Text("Welcome to Sample Universal Link App!!!")
             .padding()
     }
 }
