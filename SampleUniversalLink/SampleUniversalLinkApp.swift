fb8fe355cc8369cea173272b1fd145781511f215
Sanhanat Ngamchaiwong <sanhanat.ngam@gmail.com>
2021-01-23 12:27:12 +0700

    Initial Commit

---
+++ SampleUniversalLink/SampleUniversalLinkApp.swift
@@ -0,0 +1,17 @@
+//
+//  SampleUniversalLinkApp.swift
+//  SampleUniversalLink
+//
+//  Created by NotSmall on 23/1/2564 BE.
+//
+
+import SwiftUI
+
+@main
+struct SampleUniversalLinkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
